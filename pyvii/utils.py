56292219bf4a801fa82639209e8cb807dd26de98
Tony Pelletier <tony.pelletier@gmail.com>
2013-03-21 17:30:32 -0400

    removed unused json function in utils

--- pyvii/utils.py
+++ pyvii/utils.py
@@ -24,19 +24,3 @@ def find_key(key, item):
                 for result in find_key(key, d):
                     yield result
 
-
-def suds_to_json(suds_object):
-    # Work in progress and may ditch it.
-    json_dict = {}
-    suds_object = suds_object
-    for suds_object in suds_object:
-        if isinstance(suds_object, tuple):
-            key, value = suds_object
-            if value and isinstance(key, str):
-                for inner_key in value:
-                    if isinstance(inner_key, tuple):
-                        inner_key, inner_value = inner_key
-                        json_dict[key] = inner_value
-                    else:
-                        json_dict[key] = value
-    return json.dumps(json_dict, indent=4)
